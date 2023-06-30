dda37b8591b2979aba61a993cb2467f0b8c9515c
NqobileN <122429134+NqobileN@users.noreply.github.com>
2023-06-30 18:44:07 +0200

    Update vanilla-js.js

--- src/vanilla-js.js
+++ src/vanilla-js.js
@@ -1,1 +1,25 @@
-alert("Hello Sana");
+function displayWeather(response) {
+  console.log(response.data);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+
+  let feelsLike = document.querySelector("#feels-like");
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+
+  let cloudCover = document.querySelector("#cloud-cover");
+  cloudCover.innerHTML = response.data.clouds.all;
+
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "bff072a9132941ace910fa356cbfe57c";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Cape Town&appid=${apiKey}&units=metric`;
+
+axios.get(apiURL).then(displayWeather);
