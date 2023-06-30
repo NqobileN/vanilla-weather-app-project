79913fa4867482176c8c1e868abab2bb00586b7a
NqobileN <122429134+NqobileN@users.noreply.github.com>
2023-06-30 19:50:32 +0200

    Update vanilla-js.js

--- src/vanilla-js.js
+++ src/vanilla-js.js
@@ -1,5 +1,29 @@
+function formatDate(timestamp) {
+  let currentDate = new Date(timestamp);
+  let week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = week[currentDate.getDay()];
+
+  let currentHour = currentDate.getHours();
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+  let currentMinutes = currentDate.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+
+  return `${currentDay}, ${currentHour}:${currentMinutes}`;
+}
+
 function displayWeather(response) {
-  console.log(response.data);
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
 
@@ -17,6 +41,9 @@ function displayWeather(response) {
 
   let windSpeed = document.querySelector("#wind-speed");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+
+  let timeDate = document.querySelector("#time-date");
+  timeDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "bff072a9132941ace910fa356cbfe57c";
